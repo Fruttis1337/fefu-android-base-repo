7a780543411b814d9baf5885dbdcbf76953ac3b1
Fruttis1337 <Fruttis1337@mail.ru>
2021-10-28 08:24:20 +1000

    update welcomeScreen
    Change clickableSpan to MaterialButton

--- app/src/main/java/ru/fefu/activitytracker/WelcomeScreenActivity.kt
+++ app/src/main/java/ru/fefu/activitytracker/WelcomeScreenActivity.kt
@@ -8,6 +8,7 @@ import android.text.Spanned
 import android.text.TextPaint
 import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.core.content.ContextCompat
@@ -23,28 +24,8 @@ class WelcomeScreenActivity : AppCompatActivity() {
         binding = WelcomeScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val haveAccountText = SpannableString("Уже есть аккаунт?")
-        val haveAnAccountStringClick = object : ClickableSpan() {
-            override fun onClick(view: View) {
-                Toast.makeText(view.context, "Clicked: on Text", Toast.LENGTH_LONG).show()
-            }
-
-            override fun updateDrawState(ds: TextPaint) {
-                super.updateDrawState(ds)
-                ds.color = ResourcesCompat.getColor(resources, R.color.purple_700, null)
-                ds.linkColor = ResourcesCompat.getColor(resources, R.color.purple_700, null)
-                ds.isUnderlineText = false
-            }
+        binding.haveAnAccountMessage.setOnClickListener {
+            Log.e("TAG", "onCreate: kefbgksdjfbpgijshdf", )
         }
-        haveAccountText.setSpan(
-            haveAnAccountStringClick,
-            0,
-            haveAccountText.length,
-            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
-        )
-
-        binding.haveAnAccountMessage.text = haveAccountText
-        binding.haveAnAccountMessage.movementMethod = LinkMovementMethod.getInstance()
-        binding.haveAnAccountMessage.highlightColor = Color.TRANSPARENT
     }
 }
